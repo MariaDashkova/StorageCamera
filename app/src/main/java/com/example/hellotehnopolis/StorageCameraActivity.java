5a754d3e072b2d5f62c883e5b184066584694558
Maria Dashkova <dashkova_m@inbox.ru>
2020-02-13 13:47:31 +0300

    Add background IntentService

--- app/src/main/java/com/example/hellotehnopolis/StorageCameraActivity.java
+++ app/src/main/java/com/example/hellotehnopolis/StorageCameraActivity.java
@@ -2,29 +2,18 @@ package com.example.hellotehnopolis;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Build;
-import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
-import android.util.Log;
 
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class StorageCameraActivity extends Activity {
     private static final int REQUEST_PERMISSIONS = 100;
-    private int count =0;
-    File app_path;
-    File camera;
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -46,41 +35,16 @@ public class StorageCameraActivity extends Activity {
                         REQUEST_PERMISSIONS);
             }
         } else {
-            app_path = getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DCIM);
-            camera = new File("/storage/emulated/0/DCIM/Camera/");
-            File[] pics2 = camera.listFiles();
-            for (File pic : pics2
-            ) {
 
-                try {
-                    FileUtils.copyFileToDirectory(pic, app_path);
-                    pic.delete();
 
-                    Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cat);
-                    saveBitmap(bitmap, camera);
 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
+            // Start StorageService IntentService
+            Intent intentStorage = new Intent(this, StorageService.class);
+            startService(intentStorage);
         }
 
 
     }
 
-    private void saveBitmap(Bitmap bitmap, File path) {
-
-        try {
-            FileOutputStream fos = new FileOutputStream(path + "/cat"+count+".jpg");
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 75, fos);
-            fos.flush();
-            fos.close();
-            count++;
-        } catch (Exception e) {
-            Log.e("CatNotSaved", e.toString());
-        }
-
-    }
 
 }
